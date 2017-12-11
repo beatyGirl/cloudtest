8af7a93ed9dbf1da134282e4e9e48fbe5957db1c
tianxiaoxi <625871407@qq.com>
2017-12-11 15:45:07 +0800

    练习

---
+++ service-hi/src/main/java/com/tian/servicehi/ServiceHiApplication.java
@@ -0,0 +1,44 @@
+package com.tian.servicehi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.logging.Level;
+
+@SpringBootApplication
+@RestController
+public class ServiceHiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceHiApplication.class, args);
+	}
+
+	private static Logger logger = LoggerFactory.getLogger(ServiceHiApplication.class);
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@RequestMapping("/hi")
+	public String callHome() {
+		logger.info("calling trace service-hi");
+		return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+	}
+
+	@RequestMapping("/info")
+	public String info() {
+		logger.info("calling trace service-hi");
+		return "I am service-hi";
+	}
+}
